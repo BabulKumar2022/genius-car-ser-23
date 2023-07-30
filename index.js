55736fa08867046b68c1c859c090437066c8d9a2
unknown <bb3583400@gmail.com>
2023-07-30 19:31:07 +0600

    payment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz-lts') 
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port =process.env.PORT || 5000;
@@ -7,9 +8,11 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 
+
 //middleware
 app.use(cors());
 app.use(express.json());
+
 
 
 //verifyToken function
@@ -51,6 +54,9 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const store_id = process.env.store_id;
+const store_passwd = process.env.store_passwd;
+const is_live = false //true for live, false for sandbox
 
 async function run() {
   try {
@@ -127,51 +133,63 @@ async function run() {
     const result = await serviceCollection.deleteOne(query);
     res.send(result);
   })
-   //post for orderCollection
+   //post for orderCollection and ssl e-commerce
 
+//---------------------------------------------------------
+//    Store ID: 
+//    Store Password (API/Secret Key): 
+//----------------------------------------------------------
+const trans_id = new ObjectId().toString();
   app.post('/order', async(req, res) =>{
     const order = req.body;
-    const result = await orderCollection.insertOne(order);
-    res.send(result)
-  //   const orderedService = await serviceCollection.findOne(order)
-  //  console.log(orderedService)
-    // const data = {
-    //     total_amount: 100,
-    //     currency: 'BDT',
-    //     tran_id: 'REF123', // use unique tran_id for each api call
-    //     success_url: 'http://localhost:3030/success',
-    //     fail_url: 'http://localhost:3030/fail',
-    //     cancel_url: 'http://localhost:3030/cancel',
-    //     ipn_url: 'http://localhost:3030/ipn',
-    //     shipping_method: 'Courier',
-    //     product_name: 'Computer.',
-    //     product_category: 'Electronic',
-    //     product_profile: 'general',
-    //     cus_name: 'Customer Name',
-    //     cus_email: 'customer@example.com',
-    //     cus_add1: 'Dhaka',
-    //     cus_add2: 'Dhaka',
-    //     cus_city: 'Dhaka',
-    //     cus_state: 'Dhaka',
-    //     cus_postcode: '1000',
-    //     cus_country: 'Bangladesh',
-    //     cus_phone: '01711111111',
-    //     cus_fax: '01711111111',
-    //     ship_name: 'Customer Name',
-    //     ship_add1: 'Dhaka',
-    //     ship_add2: 'Dhaka',
-    //     ship_city: 'Dhaka',
-    //     ship_state: 'Dhaka',
-    //     ship_postcode: 1000,
-    //     ship_country: 'Bangladesh',
-    // };
-    // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-    // sslcz.init(data).then(apiResponse => {
-    //     // Redirect the user to payment gateway
-    //     let GatewayPageURL = apiResponse.GatewayPageURL
-    //     res.redirect(GatewayPageURL)
-    //     console.log('Redirecting to: ', GatewayPageURL)
-    // });
+    // const result = await orderCollection.insertOne(order);
+    // res.send(result)
+    const product = await serviceCollection.findOne({_id: new ObjectId(req.body.serviceId)})
+ 
+    const data = {
+        total_amount: product?.price,
+        currency: 'BDT',
+        tran_id: trans_id, // use unique tran_id for each api call
+        success_url: 'http://localhost:5000/success/tranId',
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: order.service,
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: order.email,
+        cus_add1: order.address,
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: order.phone,
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+    console.log(data)
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.send({url: GatewayPageURL})
+        console.log('Redirecting to: ', GatewayPageURL)
+    });
+app.post('payment/success/tranId', (req, res)=>{
+  console.log(req.params.tranId)
+})
+
+
+
   });
   
 
